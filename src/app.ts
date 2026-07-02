1a0a8fd90506de81e6f971a6f71d234a17554604
Eduardo <eduardomuller989@gmail.com>
2026-07-02 13:38:38 -0400

    chore(setup): integrate Fastify plugins and update app configuration

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,55 @@
 import Fastify, { type FastifyReply, type FastifyRequest } from 'fastify';
+import swagger from '@fastify/swagger';
+import swaggerUI from '@fastify/swagger-ui';
+import fastifyCookie from '@fastify/cookie';
+import fastifyCors from '@fastify/cors';
+import { serializerCompiler, validatorCompiler, type ZodTypeProvider } from 'fastify-type-provider-zod';
+import dbPlugin from './plugins/db.js';
+
 const app = Fastify({
     logger: true,
+}).withTypeProvider<ZodTypeProvider>();
+
+app.setValidatorCompiler(validatorCompiler);
+app.setSerializerCompiler(serializerCompiler);
+
+await app.register(fastifyCors, {
+    origin: true,
 });
 
-app.get('/', async function handler(request: FastifyRequest, reply: FastifyReply) {
+await app.register(fastifyCookie, {
+    secret: 'velunix-secret-key',
+});
+
+await app.register(dbPlugin);
+
+await app.register(swagger, {
+    openapi: {
+        info: {
+            title: 'Velunix API',
+            description: 'Documentação da API',
+            version: '1.0.0',
+        },
+    },
+});
+
+await app.register(swaggerUI, {
+    routePrefix: '/docs',
+    uiConfig: {
+        docExpansion: 'list',
+        deepLinking: false,
+    },
+});
+
+app.get('/', async function (request: FastifyRequest, reply: FastifyReply) {
     return reply.send({ hello: 'world' });
 });
 
 async function start() {
     try {
-        await app.listen({ port: 8000 });
+        if (!app.addresses().length) {
+            await app.listen({ port: 8000 });
+        }
     } catch (err) {
         app.log.error(err);
         process.exit(1);
