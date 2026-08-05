6d11c17a2403cd77ea865d02f651b0cb236346e2
Eduardo <eduardomuller989@gmail.com>
2026-08-05 18:22:40 -0400

    feat(coupons): add error handling and service functions for coupon operations

---
+++ src/modules/coupons/coupon.errors.ts
@@ -0,0 +1,23 @@
+export class CouponNotFoundError extends Error {
+    readonly code = 'COUPON_NOT_FOUND' as const;
+    constructor(readonly couponId: string) {
+        super(`Coupon not found: ${couponId}`);
+        this.name = 'CouponNotFoundError';
+    }
+}
+
+export class CouponNotCreatedError extends Error {
+    readonly code = 'COUPON_NOT_CREATED' as const;
+    constructor() {
+        super(`Coupon not created`);
+        this.name = 'CouponNotCreatedError';
+    }
+}
+
+export class CouponNotUpdatedError extends Error {
+    readonly code = 'COUPON_NOT_UPDATED' as const;
+    constructor(readonly couponId: string) {
+        super(`Coupon not updated: ${couponId}`);
+        this.name = 'CouponNotUpdatedError';
+    }
+}
